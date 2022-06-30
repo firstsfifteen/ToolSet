58abf7acec625cce7d2f9d6e4cd9a9d52d793b04
nanbei <firstsfifteen@gmail.com>
2022-06-30 15:10:12 +0800

    提交项目

---
+++ src/main/java/com/nanbei/toolset/ToolSetApplication.java
@@ -0,0 +1,13 @@
+package com.nanbei.toolset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanbei/toolset/ToolSetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanbei.toolset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
